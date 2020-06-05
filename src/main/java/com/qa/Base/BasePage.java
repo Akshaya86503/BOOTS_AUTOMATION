7e07b67ab81220762f2fedd1f51206c22054be6b
Test <Test@SWLL124.ap.corp.jwt.com>
2020-06-05 18:49:35 +0530

    New code updated

--- src/main/java/com/qa/Base/BasePage.java
+++ src/main/java/com/qa/Base/BasePage.java
@@ -20,7 +20,6 @@ public class BasePage
 			if(browserName.equals("chrome"))
 			{
 				ChromeOptions options = new ChromeOptions();
-				options.addArguments("--disable-features=VizDisplayCompositor");
 		        options.addArguments("--incognito");
 				System.setProperty("webdriver.chrome.driver", "D:/Akshaya/General/Other/eclipse/chromedriver_win32/chromedriver.exe");
 				driver = new ChromeDriver(options);
